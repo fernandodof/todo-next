7a7476866c876df69e59c9e8e025b2ba5e67ac90
Fernando Ferreira <fernandodof@gmail.com>
2021-02-16 17:26:58 -0300

    test push to deploy

--- web/src/todo/todo.model.ts
+++ web/src/todo/todo.model.ts
@@ -4,6 +4,7 @@ export class Todo {
 	@prop({ type: () => String })
 	public description?: string;
 }
+
 const TodoModel = getModelForClass(Todo, { schemaOptions: { timestamps: true } });
 
 export default TodoModel;
